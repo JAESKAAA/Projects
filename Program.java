aa42e946a18a2bf2d4c09f9ac1fc4fce1c4bdd93
Thom <81919273+leeyou31@users.noreply.github.com>
2021-11-13 16:14:26 +0900

    Upload practice by thomas
    First commit

---
+++ Program.java
@@ -0,0 +1,19 @@
+package com.gitpractice.app.util
+
+public class Program {
+     public static void main(String[]args) {
+          IntList list = new IntList();
+          list.add(3);
+          list.add(5);
+          list.clear();
+          int size = list.size();
+          System.out.printf("size : %d\n", size);
+          list add(7);
+          int num = list.get(0);
+          System.out.printf("size : %d\n", size);
+     }
+}
+/*
+* upload practice. by thomas lee
+*
+*/
